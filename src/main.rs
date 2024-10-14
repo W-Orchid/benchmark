a1910e96283d6a3c69ffffe831bf4c608f6b2440
Rumen <rumenmitov@protonmail.com>
2024-10-14 07:32:02 +0200

    feat: establish tcp connection and send dummy data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpStream;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+
+    for _ in 1..10 {
+        let buf = [1, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b];
+
+        let mut stream = TcpStream::connect("127.0.0.1:8000").unwrap();
+        stream.write(&buf).expect("Failed to write to TCP stream!");
+    }
 }
